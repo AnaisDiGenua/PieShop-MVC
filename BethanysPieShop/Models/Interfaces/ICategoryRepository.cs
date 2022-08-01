e08149e8b10a53747f6f5075d41d14fe20ad3d24
anais di genua <anais.digenua5@gmail.com>
2022-08-01 15:29:15 +0200

    add models, interfaces and mock

---
+++ BethanysPieShop/Models/Interfaces/ICategoryRepository.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace BethanysPieShop.Models.Interfaces
+{
+    public interface ICategoryRepository
+    {
+        IEnumerable<Category> AllCategories { get; }
+    }
+}
