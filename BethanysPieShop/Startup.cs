f34b5367e9d5605a4591432b96281e02694150a1
anais di genua <anais.digenua5@gmail.com>
2022-08-01 13:00:09 +0200

    add settings to startup.cs

--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -16,25 +16,33 @@ namespace BethanysPieShop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            //servizi gestiti dall'applicazione
+            //-- servizi gestiti dall'applicazione
+            services.AddControllersWithViews();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            //-- middleware components che gestiscono le chiamate http
             if (env.IsDevelopment())
             {
+                //permette di usare la pagina delle eccezioni nell'app. => informazioni utili durante lo sviluppo
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseHttpsRedirection();
+            app.UseStatusCodePages();
+            //per gestire file statici(immagini, file css, js)
+            app.UseStaticFiles();
+
+            //useRouting e useEndPoints permettono a mvc di rispondere alle richieste http
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+            endpoints.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=index}/{id?}");
             });
         }
     }
