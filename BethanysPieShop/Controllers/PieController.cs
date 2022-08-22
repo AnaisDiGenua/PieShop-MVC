b83361ca77c62844344b19a3abcd4de12dd4e627
anais di genua <anais.digenua5@gmail.com>
2022-08-22 15:13:25 +0200

    add view components

--- BethanysPieShop/Controllers/PieController.cs
+++ BethanysPieShop/Controllers/PieController.cs
@@ -1,6 +1,9 @@
-﻿using BethanysPieShop.Models.Interfaces;
+﻿using BethanysPieShop.Models.Entities;
+using BethanysPieShop.Models.Interfaces;
 using BethanysPieShop.ViewModels;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace BethanysPieShop.Controllers
 {
@@ -15,13 +18,37 @@ namespace BethanysPieShop.Controllers
             _categoryRepository = categoryRepository;
         }
 
-        public ViewResult List()
-        {
-            PiesListViewModel piesList = new PiesListViewModel();
-            piesList.Pies = _pieRepository.AllPies;
+        //public ViewResult List()
+        //{
+        //    PiesListViewModel piesList = new PiesListViewModel();
+        //    piesList.Pies = _pieRepository.AllPies;
 
-            piesList.CurrentCategory = "Cheese cake";
-            return View(piesList);
+        //    piesList.CurrentCategory = "Cheese cake";
+        //    return View(piesList);
+        //}
+
+        public ViewResult List(string category)
+        {
+            IEnumerable<Pie> pies;
+            string currentCategory;
+
+            if (string.IsNullOrEmpty(category))
+            {
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category)
+                    .OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+            }
+
+            return View(new PiesListViewModel
+            {
+                Pies = pies,
+                CurrentCategory = currentCategory
+            });
         }
 
 
