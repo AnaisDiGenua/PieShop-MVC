e75d4f3c4d677fd6254090b0c22a2b97cb8e7e64
anais di genua <anais.digenua5@gmail.com>
2022-08-23 15:23:13 +0200

    add identity package

--- BethanysPieShop/Models/AppDbContext.cs
+++ BethanysPieShop/Models/AppDbContext.cs
@@ -1,9 +1,11 @@
 ﻿using BethanysPieShop.Models.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace BethanysPieShop.Models
 {
-    public class AppDbContext : DbContext
+    public class AppDbContext : IdentityDbContext<IdentityUser>
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
